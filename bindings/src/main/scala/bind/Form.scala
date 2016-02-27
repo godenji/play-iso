07810b9dbed5b6916611d203040d16ae2088fc34
Julien Lafont <julien.lafont@tabmo.io>
2016-02-27 23:28:25 +0100

    Replace play sbt project by a simple dependency

--- bindings/src/main/scala/bind/Form.scala
+++ bindings/src/main/scala/bind/Form.scala
@@ -6,22 +6,23 @@ import play.api.data.FormError
 
 object Form {
   import macros._
+
   /**
    * materialize a value class form bindable
    */
-  implicit final 
+  implicit final
     def formBindValueClass[T <: MappedToBase]
     (implicit formBind: Formatter[T#Underlying], iso: Isomorphism[T]):
-    
+
   Formatter[T] = new Formatter[T]{
-    override def bind(key: String, data: Map[String,String]): 
+    override def bind(key: String, data: Map[String,String]):
       Seq[FormError] Either T =
         formBind.bind(key, data).fold(
           Left(_), s=> Right( iso.comap(s) )
         )
-      
-    def unbind(key: String, id: T): Map[String,String] = 
+
+    def unbind(key: String, id: T): Map[String,String] =
       formBind.unbind(key, iso.map(id) )
   }
-  
+
 }
--- project/Build.scala
+++ project/Build.scala
@@ -2,33 +2,40 @@ import sbt._
 import Keys._
 
 object ApplicationBuild extends Build {
+  val playIsoVersion = "1.1"
   val scalaVersions = Seq("2.11.7", "2.10.6")
+  val playVersion = "2.4.6"
 
   lazy val root = (project in file(".")).settings(
     name := "play-iso-build-slick",
-    version := "1.1",
+    version := playIsoVersion,
     scalacOptions ++= scalaOptionsVersion(
       scalaVersion.value, flags210 = Seq("-Yfundep-materialization"), Nil
-    ),
-    libraryDependencies ++= Seq(
-      "com.typesafe.slick" %% "slick" % "3.1.1",
-      "org.slf4j" % "slf4j-nop" % "1.7.18" % "provided",
-      "org.scala-lang" % "scala-reflect" % scalaVersion.value
     )
   )
 
   lazy val bindings = (project in file("bindings")).settings(
     name := "play-iso-slick",
-    version := "1.1",
+    version := playIsoVersion,
     crossScalaVersions := scalaVersions,
     scalaVersion in ThisBuild := scalaVersions.head,
     scalacOptions ++= scalaOptionsVersion(
       scalaVersion.value, flags210 = Seq("-Xdivergence211"), Nil
     ),
     mappings in (Compile, packageBin) ++= mappings.in(root, Compile, packageBin).value,
-    mappings in (Compile, packageSrc) ++= mappings.in(root, Compile, packageSrc).value
-  ).enablePlugins(play.sbt.PlayScala).dependsOn(root).aggregate(root)
-  
+    mappings in (Compile, packageSrc) ++= mappings.in(root, Compile, packageSrc).value,
+    resolvers in ThisBuild ++= Seq(
+      "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases/"
+    ),
+    libraryDependencies in ThisBuild ++= Seq(
+      "com.typesafe.play" %% "play" % playVersion,
+      "com.typesafe.slick" %% "slick" % "3.1.1",
+      "org.slf4j" % "slf4j-nop" % "1.7.18" % "provided",
+      "org.scala-lang" % "scala-reflect" % scalaVersion.value
+    )
+  )
+    .dependsOn(root).aggregate(root)
+
   // provide scalac flag(s) based on Scala version
   def scalaOptionsVersion(
     scalaVersion: String, flags210: Seq[String], flags211: Seq[String]
@@ -38,4 +45,16 @@ object ApplicationBuild extends Build {
       case Some((2, scalaMajor)) if scalaMajor == 11 => flags211
       case _=> Nil
     })
+
+  // Scala settings
+  scalacOptions in ThisBuild ++= Seq(
+    "-deprecation",           // Warn when deprecated API are used
+    "-feature",               // Warn for usages of features that should be importer explicitly
+    "-unchecked",             // Warn when generated code depends on assumptions
+    "-Ywarn-dead-code",       // Warn when dead code is identified
+    "-Ywarn-numeric-widen",   // Warn when numeric are widened,
+    "-Xlint",                 // Additional warnings (see scalac -Xlint:help)
+    "-Ywarn-adapted-args"     // Warn if an argument list is modified to match the receive
+  )
+
 }
